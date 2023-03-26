51ae6d15ce1c9d8644fea9d7c568b7e544fe01ad
CartoonSavage32 <75210569+CartoonSavage32@users.noreply.github.com>
2023-03-26 14:40:44 +0530

    Update readme

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ def generate_text():
     # Get the current screenshot
     screenshot = get_screenshot()
     # Initialize OpenAI API
-    openai.api_key = "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"
+    openai.api_key = "YOUR_OPENAI_API_KEY"
     model_id = "visual-chatbot-9"
     # Generate text from the screenshot using Visual ChatGPT
     response = openai.Completion.create(
